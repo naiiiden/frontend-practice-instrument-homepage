51457b4f42b3ad56473aa6c6e71e391c37605a5b
naiiiden <na4ko359@gmail.com>
2022-06-21 20:44:44 +0300

    header component add open/close menu hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import Logo from "../images/instrument.svg";
 import Menu from "../images/menu.svg";
 import Close from "../images/close.svg";
+import { useState } from "react";
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
         <header>
             <img src={Logo} alt="Instrument's logo"/>
-            <button style={{border: "none", background: "none"}}>
+            <button style={{border: "none", background: "none"}} onClick={() => setOpenMenu(!openMenu)}>
                 <img src={Menu} alt=""/>
             </button>
             <nav>
-                <ul>
+                <ul className={`${openMenu ? "show" : null}`}>
                     <li><a href="">WHAT WE DO</a></li>
                     <li><a href="">WHO WE ARE</a></li>
                     <li><a href="">BEING HERE</a></li>
