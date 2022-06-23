3ca241a2e2efc76d5677a8abd6213c9cf984cd28
naiiiden <na4ko359@gmail.com>
2022-06-23 21:11:53 +0300

    hero component embed vimeo video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,6 +22,9 @@ const Hero = () => {
             </h1>
             <p>We are investing $3 million over the next three years in our Build|Grow|Serve program, created to support and empower Black and underrepresented communities.</p>
             <a href="#">Learn more <img src={Arrow} alt=""/></a>
+            <div className="video--player">
+                <iframe src="https://player.vimeo.com/video/428281636?h=be0963aaa9" frameBorder={0}/>
+            </div>
         </main>
     )
 }
