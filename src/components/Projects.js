da15aa8cb67fabd21939144e7b01cc3251a2f3aa
naiiiden <na4ko359@gmail.com>
2022-06-24 22:45:47 +0300

    projects component update margin/padding on 500px+

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,13 +12,17 @@ const Projects = () => {
             <div className="projects--works--container">
                 <a className="projects--work">
                     <img src={dec1} alt=""/>
-                    <h3 className="work--heading">Eames Institute</h3>
-                    <p>Ray and Charles Eames laid the foundation for modern design and created work that has transcended time. With an aspiration to bring the Eameses’ timeless methodologies to a modern audience, the Eames Institute enlisted Instrument to co-create a digital platform to unveil their vast collection to the world.</p>
+                    <div className="text--container">
+                        <h3 className="work--heading">Eames Institute</h3>
+                        <p>Ray and Charles Eames laid the foundation for modern design and created work that has transcended time. With an aspiration to bring the Eameses’ timeless methodologies to a modern audience, the Eames Institute enlisted Instrument to co-create a digital platform to unveil their vast collection to the world.</p>
+                    </div>
                 </a>
                 <a className="projects--work">
                     <img src={dec3} alt=""/>
-                    <h3 className="work--heading">Dropbox "For All Things Worth Saving"</h3>
-                    <p>To evolve and mature how Dropbox engages with their customers, we developed a multi-year brand platform as well as launched its first campaign titled 'For All Things Worth Saving' — an ecosystem of branded experiences aimed to remind people of the inherent value of their digital files.</p>
+                    <div className="text--container">
+                        <h3 className="work--heading">Dropbox "For All Things Worth Saving"</h3>
+                        <p>To evolve and mature how Dropbox engages with their customers, we developed a multi-year brand platform as well as launched its first campaign titled 'For All Things Worth Saving' — an ecosystem of branded experiences aimed to remind people of the inherent value of their digital files.</p>
+                    </div>
                 </a>
             </div>
         </section>
