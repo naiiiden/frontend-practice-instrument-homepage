a523721402e36fdc83a8c12c1532c8cfdc5b8b3f
naiiiden <na4ko359@gmail.com>
2022-06-24 22:59:40 +0300

    projects component styled on desktop

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,17 +7,17 @@ const Projects = () => {
         <section className="projects--section">
             <div className="projects--heading--work--container">
                 <h2 className="projects--heading">Explore a few of our most impactful projects</h2>
-                <a href="#">Our work <img src={arrow} alt=""/></a>
+                <a href="https://www.instrument.com/work">Our work <img src={arrow} alt=""/></a>
             </div>
             <div className="projects--works--container">
-                <a className="projects--work">
+                <a className="projects--work" href="https://www.instrument.com/work/eames-institute">
                     <img src={dec1} alt=""/>
                     <div className="text--container">
                         <h3 className="work--heading">Eames Institute</h3>
                         <p>Ray and Charles Eames laid the foundation for modern design and created work that has transcended time. With an aspiration to bring the Eameses’ timeless methodologies to a modern audience, the Eames Institute enlisted Instrument to co-create a digital platform to unveil their vast collection to the world.</p>
                     </div>
                 </a>
-                <a className="projects--work">
+                <a className="projects--work" href="https://www.instrument.com/work/dropbox-award-submission">
                     <img src={dec3} alt=""/>
                     <div className="text--container">
                         <h3 className="work--heading">Dropbox "For All Things Worth Saving"</h3>
