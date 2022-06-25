4aa5ba0eb07912261b249df9bb1801ef1b26c0ac
naiiiden <na4ko359@gmail.com>
2022-06-25 23:00:42 +0300

    partners component animation cleanup/each row has same number of images

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,13 +1,11 @@
 import airbnb from "../images/airbnb.png";
 import better from "../images/better.png";
 import dropbox from "../images/dropbox.png";
-import dwell from "../images/dwell.png";
 import ebay from "../images/ebay.png";
 import facebook from "../images/facebook.png";
 import google from "../images/google.png";
-import gopro from "../images/gopro.png";
+import dwell from "../images/dwell.png";
 import intercom from "../images/intercom.png";
-import levi from "../images/levi.png";
 import linkedin from "../images/linkedin.png";
 import mercy_corps from "../images/mercy_corps.png";
 import netflix from "../images/netflix.png";
@@ -26,21 +24,21 @@ import umpqua from "../images/umpqua.png";
 import xbox from "../images/xbox.png";
 import yeti from "../images/yeti.png";
 import youtube from "../images/youtube.png";
+import levi from "../images/levi.png";
 
 const Partners = () => {
     return (
         <div className="partners--container">
             <div className="row row1">
-                <img src={airbnb} alt="" className="img img11 imgrow1"/>
-                <img src={better} alt="" className="img img12 imgrow1"/>
-                <img src={dropbox} alt="" className="img img13 imgrow1"/>
-                <img src={dwell} alt="" className="img img14 imgrow1"/>
-                <img src={ebay} alt="" className="img img15 imgrow1"/>
-                <img src={facebook} alt="" className="img img16 imgrow1"/>
-                <img src={google} alt="" className="img img17 imgrow1"/>
-                <img src={gopro} alt="" className="img img18 imgrow1"/>
-                <img src={intercom} alt="" className="img img19 imgrow1"/>
-                <img src={levi} alt="" className="img img20 imgrow1"/>
+                <img src={levi} alt="" className="img"/>
+                <img src={airbnb} alt="" className="img img11"/>
+                <img src={better} alt="" className="img img12"/>
+                <img src={dropbox} alt="" className="img img13"/>
+                <img src={ebay} alt="" className="img img14"/>
+                <img src={facebook} alt="" className="img img15"/>
+                <img src={google} alt="" className="img img16"/>
+                <img src={dwell} alt="" className="img img17"/>
+                <img src={intercom} alt="" className="img img18"/>            
             </div>
             <div className="row row2">
                 <img src={linkedin} alt="" className="img"/>
