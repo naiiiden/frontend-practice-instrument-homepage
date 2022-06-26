d674e1246861a0a438fd1a2f52bb77824c299bb3
naiiiden <na4ko359@gmail.com>
2022-06-26 13:45:09 +0300

    cta component and fade animation on reveal

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,8 +1,10 @@
 import arrow from "../images/arrow.svg";
+import "animate.css";
+import WOW from "wow.js";
 
 const Cta = () => {
     return (
-        <section className="cta--section">
+        <section className="cta--section wow animate__animated animate__fadeIn">
             <h2 className="cta--heading">We’d love to be your partner</h2>
             <a href="https://www.instrument.com/contact">Get in touch <img src={arrow} alt=""/></a>
         </section>
