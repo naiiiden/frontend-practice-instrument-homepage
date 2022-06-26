a8409bd5319fc255057b2716e10f045b0d8ab3dc
naiiiden <na4ko359@gmail.com>
2022-06-26 12:52:21 +0300

    cta component

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,6 +1,11 @@
+import arrow from "../images/arrow.svg";
+
 const Cta = () => {
     return (
-        <p>hey</p>
+        <section className="cta--section">
+            <h2 className="cta--heading">We’d love to be your partner</h2>
+            <a href="https://www.instrument.com/contact">Get in touch <img src={arrow} alt=""/></a>
+        </section>
     )
 }
 
