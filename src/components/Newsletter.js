f7aa2287f1e5668fe4d568a7317c1a04ad95dfe4
naiiiden <na4ko359@gmail.com>
2022-06-28 20:18:58 +0300

    newsletter modal component

---
+++ src/components/Newsletter.js
@@ -0,0 +1,23 @@
+import at from "../images/at.svg";
+import arrow from "../images/arrow__footer.svg";
+import close from "../images/newsletter_close.svg";
+
+const Newsletter = ({ onClick }) => {
+    return (
+        <section className="newsletter--modal">
+            <div className="heading--close_btn--container">
+                <div className="at--heading--container">
+                    <img src={at} alt=""/>
+                    <h3>Join Our Newsletter</h3>
+                </div>
+                <button onClick={onClick}><img src={close} alt=""/></button>
+            </div>
+            <form className="newsletter--form">
+                <input type="email" placeholder="Enter Email Address..."/>
+                <input type="submit" value={"Subscribe" + <img src={arrow} alt=""/>}/>
+            </form>
+        </section>
+    )
+}
+
+export default Newsletter;
