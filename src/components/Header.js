2fa65d84422be2ce221e5dd8a1b6d3c9b6b9cf4b
naiiiden <na4ko359@gmail.com>
2022-06-29 13:59:35 +0300

    header component light mode completed on mobile & mobile menu/delete unused imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import Logo from "../images/instrument.svg";
-import Menu from "../images/menu.svg";
-import Close from "../images/close.svg";
 import { useState } from "react";
+import { ReactComponent as Logo } from "../images/instrument.svg";
+import { ReactComponent as Menu } from "../images/menu.svg";
+import { ReactComponent as Close } from "../images/close.svg";
+
 
 const Header = ({ darkMode, toggleDarkMode }) => {
     const [openMenu, setOpenMenu] = useState(false);
@@ -19,9 +20,11 @@ const Header = ({ darkMode, toggleDarkMode }) => {
 
     return (
         <header className={`header ${darkMode ? "" : "header--light"}`} id="header">
-            <img src={Logo} alt="Instrument's logo" className="logo"/>
+            {/* <img src={Logo} alt="Instrument's logo" className="logo"/> */}
+            <Logo className="logo" alt="Instrument's logo"/>
             <button className="menu--button" onClick={() => setOpenMenu(!openMenu)}>
-                <img src={!openMenu ? Menu : Close} alt=""/>
+                {/* <img src={!openMenu ? Menu : Close} alt=""/> */}
+                {!openMenu ? <Menu className="menu--icon"/> : <Close className="menu--icon"/>}
             </button>
             <button onClick={toggleDarkMode}>TEST</button>
             <nav>
