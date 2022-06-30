7229b15c5a27f3f06112956d5c691ac896c385fe
naiiiden <na4ko359@gmail.com>
2022-06-30 07:31:54 +0300

    cta component styled on light mode

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -2,11 +2,11 @@ import "animate.css";
 import WOW from "wow.js";
 import BtnLink from "./Button";
 
-const Cta = () => {
+const Cta = ({ darkMode }) => {
     new WOW().init();
 
     return (
-        <section className="cta--section wow animate__animated animate__fadeIn">
+        <section className={`cta--section wow animate__animated animate__fadeIn ${darkMode ? "" : "cta--light"}`}>
             <h2 className="cta--heading">We’d love to be your partner</h2>
             <BtnLink href="https://www.instrument.com/contact" text="Get in touch"/>
         </section>
