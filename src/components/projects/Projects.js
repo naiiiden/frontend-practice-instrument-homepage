bcfa230715161f3858ebcd3e312566e741b9db02
naiiiden <na4ko359@gmail.com>
2024-05-29 10:31:21 +0300

    dynamify projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,26 +7,42 @@ import art2 from "../../images/art2.jpg";
 
 const Projects = ({ darkMode }) => {
     new WOW().init();
+
+    const data = {
+        "headingText": "Explore a few of our most impactful projects",
+        "linkText": "Our work",
+        "projects": [
+            {
+                "href": "https://www.instrument.com/work/eames-institute",
+                "src": art1,
+                "headingText": "Eames Institute",
+                "pText": "Ray and Charles Eames laid the foundation for modern design and created work that has transcended time. With an aspiration to bring the Eameses’ timeless methodologies to a modern audience, the Eames Institute enlisted Instrument to co-create a digital platform to unveil their vast collection to the world."
+            },
+            {
+                "href": "https://www.instrument.com/work/dropbox-award-submission",
+                "src": art2,
+                "headingText": "Dropbox &quot;For All Things Worth Saving&quot;",
+                "pText": "To evolve and mature how Dropbox engages with their customers, we developed a multi-year brand platform as well as launched its first campaign titled 'For All Things Worth Saving' — an ecosystem of branded experiences aimed to remind people of the inherent value of their digital files."
+            }
+        ]
+    }
     
     return (
         <section className={`projects--section ${darkMode ? "" : "projects--light"}`}>
             <div className="projects--heading--work--container wow animate__animated animate__fadeIn" data-wow-offset="200">
-                <h2 className="projects--heading">Explore a few of our most impactful projects</h2>
-                <BtnLink href="https://www.instrument.com/work" text="Our work" className={`${darkMode ? "" : "btn--light"}`}/>
+                <h2 className="projects--heading">{data.headingText}</h2>
+                <BtnLink href="https://www.instrument.com/work" text={data.linkText} className={`${darkMode ? "" : "btn--light"}`}/>
             </div>
             <div className="projects--works--container wow animate__animated animate__fadeIn" data-wow-offset="300">
-                <SingleProject
-                    href="https://www.instrument.com/work/eames-institute"
-                    src={art1}
-                    h3="Eames Institute"
-                    p="Ray and Charles Eames laid the foundation for modern design and created work that has transcended time. With an aspiration to bring the Eameses’ timeless methodologies to a modern audience, the Eames Institute enlisted Instrument to co-create a digital platform to unveil their vast collection to the world."
-                />
-                <SingleProject
-                    href="https://www.instrument.com/work/dropbox-award-submission"
-                    src={art2}
-                    h3="Dropbox &quot;For All Things Worth Saving&quot;"
-                    p="To evolve and mature how Dropbox engages with their customers, we developed a multi-year brand platform as well as launched its first campaign titled 'For All Things Worth Saving' — an ecosystem of branded experiences aimed to remind people of the inherent value of their digital files."
-                />
+                {data.projects.map((item, i) => (
+                    <SingleProject
+                        key={i}
+                        href={item.href}
+                        src={item.src}
+                        h3={item.headingText}
+                        p={item.pText}
+                    />
+                ))}
             </div>
         </section>
     )
